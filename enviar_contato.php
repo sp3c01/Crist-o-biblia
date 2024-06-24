1f228dab27388d0e7b32cd7cb13d24e5e1e75c02
sp3c01 <163803149+sp3c01@users.noreply.github.com>
2024-06-24 17:00:53 -0300

    Update enviar_contato.php

--- enviar_contato.php
+++ enviar_contato.php
@@ -1,13 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = htmlspecialchars(trim($_POST["nome"]));
-    $email = htmlspecialchars(trim($_POST["email"]));
     $mensagem = htmlspecialchars(trim($_POST["mensagem"]));
 
     $to = "darknetpirates@gmail.com";
     $subject = "Mensagem de Contato do Site Cristão";
-    $body = "Nome: $nome\nEmail: $email\nMensagem:\n$mensagem";
-    $headers = "From: $email";
+    $body = "Mensagem:\n$mensagem";
+    $headers = "From: noreply@seusite.com\r\n";
+    $headers .= "Reply-To: noreply@seusite.com\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
     if (mail($to, $subject, $body, $headers)) {
         header("Location: obrigado.html");
